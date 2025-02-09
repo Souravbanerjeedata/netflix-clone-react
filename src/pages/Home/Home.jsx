fcc17a3c8396226040cedb463fe541fb15eb78c5
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2025-02-10 02:05:38 +0530

    built hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import heroBanner from "../../assets/hero_banner.jpg";
+import herotitle from "../../assets/hero_title.png";
+import playIcon from "../../assets/play_icon.png";
+import infoIcon from "../../assets/info_icon.png";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={heroBanner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={herotitle} alt=" " className="caption-img" />
+          <p>
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={playIcon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={infoIcon} alt="" />
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
